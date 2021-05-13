3d98e866ba927aaf5e19b820866db40e3e9e5fe7
Masterchef365 <duncan.freeman1@gmail.com>
2021-05-13 02:46:15 -0700

    The great exodus

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,46 @@
+pub mod framebuffer_mgr;
+pub mod frame_data_ubo;
+pub mod render_pass;
+pub mod shader;
+pub mod staging_buffer;
+pub mod synchronization;
+pub mod vertex;
 pub mod app_info;
 pub mod core;
+pub mod defaults;
 pub mod hardware_query;
+pub mod memory;
+pub mod mesh;
+
+pub mod prelude {
+    pub use super::{
+        framebuffer_mgr::FramebufferManager,
+        frame_data_ubo::FrameDataUbo,
+        memory::{buffer_memory_req, image_memory_req, ManagedBuffer, ManagedImage, MemoryBlock},
+        render_pass::create_render_pass,
+        shader::shader,
+        staging_buffer::StagingBuffer,
+        synchronization::Synchronization,
+        vertex::Vertex,
+    };
+}
+
+#[cfg(feature = "nalgebra")]
+pub mod arcball;
+
+#[cfg(all(feature = "nalgebra", feature = "winit"))]
+pub mod winit_arcball;
+
+#[cfg(all(feature = "nalgebra", feature = "openxr"))]
+pub mod xr_camera;
+
+#[cfg(all(feature = "nalgebra", feature = "openxr", feature = "winit"))]
+mod multi_platform_camera;
+#[cfg(all(feature = "nalgebra", feature = "openxr", feature = "winit"))]
+pub use multi_platform_camera::MultiPlatformCamera;
+
+#[cfg(all(feature = "nalgebra", feature = "openxr", feature = "winit"))]
+pub mod starter_kit;
 
 /// Vulkan implementation supplied by Erupt
 pub use erupt::vk;
@@ -22,20 +62,7 @@ pub mod mainloop;
 #[cfg(feature = "nalgebra")]
 pub use nalgebra;
 
-pub mod shortcuts;
-
 /// Go figure
 pub const ENGINE_NAME: &str = "WaterTender";
-
-pub mod prelude {
-    pub use super::*;
-    #[cfg(all(feature = "openxr", feature = "winit"))]
-    pub use mainloop::{
-        Frame, MainLoop, Platform, PlatformEvent, PlatformReturn, SyncMainLoop,
-    };
-    pub use {app_info::AppInfo, hardware_query::HardwareSelection};
-    pub use shortcuts::memory::{MemoryBlock, ManagedImage, ManagedBuffer, UsageFlags};
-    pub use {SharedCore, Core};
-}
 
 pub use crate::core::{Core, SharedCore};
