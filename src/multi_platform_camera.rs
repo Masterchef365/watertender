8b1e94e5892f6897035bcfe76953a646e3c97d5e
Masterchef365 <duncan.freeman1@gmail.com>
2021-11-10 23:46:30 -0800

    Begin adjusting multiplatform camera

--- src/multi_platform_camera.rs
+++ src/multi_platform_camera.rs
@@ -2,6 +2,7 @@ use crate::mainloop::{Platform, PlatformEvent, PlatformReturn};
 use crate::winit_arcball::WinitArcBall;
 use crate::xr_camera;
 use anyhow::Result;
+use nalgebra::Matrix4;
 
 pub enum MultiPlatformCamera {
     Winit(WinitArcBall),
@@ -20,46 +21,11 @@ impl MultiPlatformCamera {
     }
 
     pub fn get_matrices(&self, platform: &Platform) -> Result<(PlatformReturn, [f32; 4 * 4 * 2])> {
-        match (self, platform) {
-            // Winit mode
-            #[cfg(feature = "winit")]
-            (Self::Winit(winit_arcball), Platform::Winit { .. }) => {
-                let matrix = winit_arcball.matrix();
-                let mut data = [0.0; 32];
-                data.iter_mut()
-                    .zip(matrix.as_slice().iter())
-                    .for_each(|(o, i)| *o = *i);
-                Ok((PlatformReturn::Winit, data))
-            }
-            // OpenXR mode
-            #[cfg(feature = "openxr")]
-            (
-                Self::OpenXr,
-                Platform::OpenXr {
-                    xr_core,
-                    frame_state,
-                },
-            ) => {
-                let (_, views) = xr_core.session.locate_views(
-                    openxr::ViewConfigurationType::PRIMARY_STEREO,
-                    frame_state.expect("No frame state").predicted_display_time,
-                    &xr_core.stage,
-                )?;
-                let view_to_mat = |view: openxr::View| {
-                    let proj = xr_camera::projection_from_fov(&view.fov, 0.01, 1000.0); // TODO: Settings?
-                    let view = xr_camera::view_from_pose(&view.pose);
-                    proj * view
-                };
-                let left = view_to_mat(views[0]);
-                let right = view_to_mat(views[1]);
-                let mut data = [0.0; 32];
-                data.iter_mut()
-                    .zip(left.as_slice().iter().chain(right.as_slice().iter()))
-                    .for_each(|(o, i)| *o = *i);
-                Ok((PlatformReturn::OpenXr(views), data))
-            }
-            _ => panic!("{}", PLATFORM_WARNING),
-        }
+        let prefix = match self {
+            Self::Winit(arcball) => arcball.matrix(),
+            Self::OpenXr => Matrix4::identity(),
+        };
+        platform_camera_prefix(platform, bytemuck::cast(*prefix.as_ref()))
     }
 
     pub fn handle_event(
@@ -75,6 +41,44 @@ impl MultiPlatformCamera {
             }
             (Self::OpenXr, PlatformEvent::OpenXr(_)) => (),
             _ => panic!("{}", PLATFORM_WARNING),
+        }
+    }
+}
+
+/// Create the specified PlatformReturn and return camera matrices for one or both eyes, prefixed with the given 4x4 matrix
+pub fn platform_camera_prefix(platform: &Platform, prefix: [f32; 4 * 4]) -> Result<(PlatformReturn, [f32; 4 * 4 * 2])> {
+    match platform {
+        // Winit mode
+        #[cfg(feature = "winit")]
+        Platform::Winit { .. } => {
+            let mut data = [0.0; 4 * 4 * 2];
+            data[..prefix.len()].copy_from_slice(&prefix);
+            Ok((PlatformReturn::Winit, data))
+        }
+        // OpenXR mode
+        #[cfg(feature = "openxr")]
+        Platform::OpenXr {
+            xr_core,
+            frame_state,
+        } => {
+            let prefix = Matrix4::from_row_slice(&prefix);
+            let (_, views) = xr_core.session.locate_views(
+                openxr::ViewConfigurationType::PRIMARY_STEREO,
+                frame_state.expect("No frame state").predicted_display_time,
+                &xr_core.stage,
+            )?;
+            let view_to_mat = |view: openxr::View| {
+                let proj = xr_camera::projection_from_fov(&view.fov, 0.01, 1000.0); // TODO: Settings?
+                let view = xr_camera::view_from_pose(&view.pose);
+                proj * view * prefix
+            };
+            let left = view_to_mat(views[0]);
+            let right = view_to_mat(views[1]);
+            let mut data = [0.0; 32];
+            data.iter_mut()
+                .zip(left.as_slice().iter().chain(right.as_slice().iter()))
+                .for_each(|(o, i)| *o = *i);
+            Ok((PlatformReturn::OpenXr(views), data))
         }
     }
 }
